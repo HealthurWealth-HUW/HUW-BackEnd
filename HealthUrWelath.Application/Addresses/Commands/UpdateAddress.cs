c4a845cafa1344caf61a654f4d25ca63adbf3e54
yelagala <https://github.com/HealthurWealth-HUW/backend.git>
2026-05-20 11:36:25 +0530

    Handling AddressType as instead of 0 , make it 1( Billing)

--- HealthUrWelath.Application/Addresses/Commands/UpdateAddress.cs
+++ HealthUrWelath.Application/Addresses/Commands/UpdateAddress.cs
@@ -1,6 +1,7 @@
 ﻿using HealthUrWelath.Application.Addresses.Dtos;
 using HealthUrWelath.Application.Addresses.Interfaces;
 using HealthUrWelath.Application.Authentication.Interfaces;
+using HealthUrWelath.Application.Common.Exceptions;
 using MediatR;
 
 namespace HealthUrWelath.Application.Addresses.Commands
@@ -22,7 +23,18 @@ namespace HealthUrWelath.Application.Addresses.Commands
 
             public async Task Handle(Command request, CancellationToken ct)
             {
-                await _repo.UpdateAsync(request.Address, _currentUser.UserId);
+                // If UI didn't provide AddressType (0), default to Billing (1)
+                var addr = request.Address;
+                if (addr.AddressTypeId == 0)
+                {
+                    addr = addr with { AddressTypeId = 1 };
+                }
+
+                // Ensure AddressType is valid (Billing = 1, Shipping = 2)
+                if (addr.AddressTypeId != 1 && addr.AddressTypeId != 2)
+                    throw new AppException("AddressType is required and must be Billing(1) or Shipping(2)");
+
+                await _repo.UpdateAsync(addr, _currentUser.UserId);
             }
         }
     }
